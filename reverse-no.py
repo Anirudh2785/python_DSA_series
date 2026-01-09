7b94b13d7f43910c4f4b04d6513552d06a3361db
Anirudh2785 <anirudhpawarray440@gmail.com>
2026-01-09 18:07:25 +0530

    Update reverse-no.py

--- reverse-no.py
+++ reverse-no.py
@@ -1,3 +1,12 @@
-no = input("enter number :")
-no = (list(map(int,no))) 
-print(no[::-1])
+# no = input("enter number :")
+# no = (list(map(int,no))) 
+# print(no[::-1])
+
+n = int(input("enter number : "))
+
+while n > 0 :
+    b = 0
+    a = n % 10  
+    b = b * 10 + a 
+
+print("So the no. : ")
