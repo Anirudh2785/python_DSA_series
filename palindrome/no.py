31e1ad7c9d44b41f4a5005b1fe0b558a53341ac8
Anirudh2785 <anirudhpawarray440@gmail.com>
2026-01-09 19:30:55 +0530

    Update no.py

--- palindrome/no.py
+++ palindrome/no.py
@@ -5,9 +5,11 @@ b = 0
 
 while a > 0:
     n = a % 10 
-    b = b*10+n
-    a//=10
-if a == b :
-    print("True,",no)
+    b = b * 10 + n
+    a //= 10
+if no == b :
+    print("True,",no,"is a palindrome num" )
+elif no != b :
+    print("False,",no,"is a palindrome num")
 else :
-     print("False,",no)
+    print("error")
