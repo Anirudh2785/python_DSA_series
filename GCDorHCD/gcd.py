97770add9179ff22dbade1f600ad182bf69e2e8e
Anirudh2785 <anirudhpawarray440@gmail.com>
2026-01-19 15:57:13 +0530

    Update gcd.py

--- GCDorHCD/gcd.py
+++ GCDorHCD/gcd.py
@@ -16,20 +16,22 @@ for i in range (1,n1+1) :
         else :
             up.append(i)
             up.append(div1)
-            for o in range (1,n2+1) :
-                rem2= num2%o
-                if rem2 == 0 :
-                    div2 = int(num2/o)
-                    if div2 == o :
-                        down.append(o)
-                    else :
-                        down.append(o)
-                        down.append(div2)
+
+for o in range (1,n2+1) :
+    rem2= num2%o
+    if rem2 == 0 :
+        div2 = int(num2/o)
+        if div2 == o :
+            down.append(o)
+        else :
+            down.append(o)
+            down.append(div2)
     else :
         print(end="")
 
-up.sort()
-down.sort()
+com = set(up) & set(down)
 
-if up == down :
-    print(up , end=" ")
+print(com)
+
+
+
