84cd17055df3d2ae2eae4374de081f887b356032
Anirudh2785 <anirudhpawarray440@gmail.com>
2026-01-22 18:24:34 +0530

    Update trangal_pattern.py

--- patterns/trangal_pattern.py
+++ patterns/trangal_pattern.py
@@ -3,7 +3,7 @@ i = 0
 while i <= 4 :
     j = 4 - i 
     while j > 0 :
-        print ("  ",end="")
+        print (end="  ")
         j -= 1
     k = 0 
     while k <= a :
@@ -11,4 +11,4 @@ while i <= 4 :
         k += 1
     print()
     i += 1
-    a += 2
+    a += 2 
