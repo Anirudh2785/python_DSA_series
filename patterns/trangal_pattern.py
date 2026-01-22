7945fb60e685b584f63b6dd1c62083c7f7122183
Anirudh2785 <anirudhpawarray440@gmail.com>
2026-01-22 18:19:26 +0530

    Create trangal_pattern.py

---
+++ patterns/trangal_pattern.py
@@ -0,0 +1,14 @@
+a = 0
+i = 0
+while i <= 4 :
+    j = 4 - i 
+    while j > 0 :
+        print ("  ",end="")
+        j -= 1
+    k = 0 
+    while k <= a :
+        print ( "*", end=" ")
+        k += 1
+    print()
+    i += 1
+    a += 2
